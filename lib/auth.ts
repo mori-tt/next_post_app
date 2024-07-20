565e14935fecbb54ddcd27186b0928f5d12280dd
T_Mori <morit0301@gmail.com>
2024-07-20 12:30:03 +0900

    💄 Prisma/Supabase

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,23 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "./db";
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
+  adapter: PrismaAdapter(db),
   pages: { signIn: "/login" },
   callbacks: {
     async session({ token, session }) {
@@ -25,5 +29,9 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
+  },
+  // debug: true, // デバッグを有効にする
+  session: {
+    strategy: "jwt",
   },
 };
