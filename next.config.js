6053c1d97a3eef60c0a9174deddccbe5d89c3399
T_Mori <morit0301@gmail.com>
2024-07-23 03:13:34 +0900

    🚀 requireに変更/prisma build変更

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
-const withContentlayer = require("next-contentlayer");
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true, swcMinify: true };
 
-export default withContentlayer(nextConfig);
+module.expoets = withContentlayer(nextConfig);
