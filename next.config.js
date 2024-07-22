822f122ed1c38654c90bd0d3bab36476e1234a8e
T_Mori <morit0301@gmail.com>
2024-07-23 03:10:51 +0900

    🚀 import→require

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // next.config.js
-import { withContentlayer } from "next-contentlayer";
+const withContentlayer = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true, swcMinify: true };
